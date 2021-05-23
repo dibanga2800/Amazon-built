3f04cd6ef8f2a2158f2d08168fe4e714133b002b
dibanga200 <dibanga2800@gmail.com>
2021-05-23 14:22:45 +0100

    Day3

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDrhP8zvOMRYAOOEcBUVk116GCLLR2PF4Q",
+  authDomain: "amzn-clone-fc221.firebaseapp.com",
+  projectId: "amzn-clone-fc221",
+  storageBucket: "amzn-clone-fc221.appspot.com",
+  messagingSenderId: "768186258036",
+  appId: "1:768186258036:web:5b0865855920ac1d06f721",
+};
